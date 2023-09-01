67c86c8d408b3597467b4c540eba3a6d6016e376
abeertech01 <abeer.technology@gmail.com>
2023-09-01 10:19:55 +0600

    little change in contact page

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -3,25 +3,15 @@ import { BsArrowRight } from "react-icons/bs"
 
 const Contact = () => {
   return (
-    <div className="h-full bg-primary/30">
+    <div className="h-full">
       <div className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
-        <div className="flex flex-col w-full max-w-[700px]">
+        <div className="flex flex-col w-full max-w-[70rem]">
           {/* text & form */}
-          <h2
-            initial="hidden"
-            animate="show"
-            exit="hidden"
-            className="h2 text-center mb-12"
-          >
-            Let's <span className="text-accent">connect.</span>
+          <h2 className="h2 text-center mb-12">
+            Let's <span className="">connect.</span>
           </h2>
           {/* form */}
-          <form
-            initial="hidden"
-            animate="show"
-            exit="hidden"
-            className="flex-1 flex flex-col gap-6 w-full mx-auto"
-          >
+          <form className="flex-1 flex flex-col gap-6 w-full mx-auto">
             {/* input group */}
             <div className="flex gap-x-6 w-full">
               <input type="text" placeholder="name" className="input" />
@@ -33,7 +23,7 @@ const Contact = () => {
               <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
                 Let's talk
               </span>
-              <BsArrowRight className="-translate-y-[120%] opacity-0 group-hover:felx group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]" />
+              <BsArrowRight className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]" />
             </button>
           </form>
         </div>
