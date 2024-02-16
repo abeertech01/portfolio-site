af063d56864b479f21d91b9cfeb2044007b287c0
abeertech01 <abeer.technology@gmail.com>
2024-02-17 04:52:38 +0600

    final

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
 import "./style.css"
+
+document.addEventListener("DOMContentLoaded", function () {
+  let tabs = document.querySelectorAll(".tab")
+  let tabBtns = document.querySelectorAll(".tab-btn")
+
+  tabBtns.forEach((button) => {
+    button.addEventListener("click", function () {
+      let targetId = button.getAttribute("data-target")
+      let targetElement = document.querySelector(targetId)
+
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+      })
+    })
+  })
+
+  window.addEventListener("scroll", function () {
+    let scrollPosition = window.scrollY
+
+    tabs.forEach((tab, i) => {
+      let tabTop = tab.offsetTop
+      let tabBottom = tabTop + tab.clientHeight
+
+      if (scrollPosition >= tabTop && scrollPosition <= tabBottom) {
+        tabBtns[i].classList.add("active")
+      } else {
+        tabBtns[i].classList.remove("active")
+      }
+    })
+  })
+})
